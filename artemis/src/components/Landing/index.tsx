70089baf52577c27e8d8006c5d168027398866db
LaylaASerrano <lserrano03@tamu.edu>
2025-10-18 22:43:32 -0500

    added landing

--- artemis/src/components/Landing/index.tsx
+++ artemis/src/components/Landing/index.tsx
@@ -1,12 +1,13 @@
+"use client";
 import Image from "next/image";
-
+import Link from "next"
 export default function LandingPage(){
     return(
         <div className="relative grid grid-cols-10 [grid-template-rows:repeat(10,1fr)] md:[grid-template-rows:repeat(14,80px)] min-h-screen sm:min-h-fit w-full overflow-hidden pb-8">
             {/* Gradient overlay */}
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent from-[#36336A] via-transparent to-[#724C6A]" />
+            <div className="absolute inset-0 bg-gradient-to-b from-[#36336A] via-[#5a5080] to-[#7a506a]" />
 
-            <div className="relative z-10 col-start-1 col-span-10 row-start-1 row-span-8 flex items-top justify-center transform translate-y-4 -translate-x-1/2" >
+            <div className="absolute z-20 left-0 top-4 -translate-x-1/2" >
                 <Image
                     src="/images/starL.svg"
                     alt="stars on the left side"
@@ -16,8 +17,31 @@ export default function LandingPage(){
                 />
             </div>
 
+            <div className="absolute z-20 right-0 top-4 translate-x-1/2" >
+                <Image
+                    src="/images/starR.svg"
+                    alt="stars on the right side"
+                    width={600}
+                    height={600}
+                    className="object-contain"
+                />
+            </div>
+
+            <div className="absolute z-20 left-1/2 top-[45%] -translate-x-1/2 -translate-y-1/2">
+                <Image
+                    src="/images/bunny.svg"
+                    alt="bunny logo"
+                    width={600}
+                    height={600}
+                    className="object-fill"
+
+                />
+                <p className="font-mono text-2xl text-[#FFE8B3] text-center ">
+                    Hello, My name is Artemis
+                </p>
+
+            </div>
             
         </div>
-
     )
 }
